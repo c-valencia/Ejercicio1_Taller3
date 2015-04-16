8f4fc147bbc76a989b5c9d6da0280e60babf6f5e
Oscar Steven Romero Beron <romero.oscar95@gmail.com>
2015-04-15 23:02:24 -0500

    Creacion archivos base

---
+++ Ejercicio_1/src/Logica.java
@@ -0,0 +1,10 @@
+/** 
+ * Nombre del Archivo: Logica.java
+ * Autores: JULIAN GARCIA RICO (1225435) 
+ *          DIEGO FERNANDO BEDOYA (1327749) 
+ *          CRISTIAN ALEXANDER VALENCIA TORRES (1329454) 
+ *          OSCAR STEVEN ROMERO BERON (1326750) 
+ */
+public class Logica {
+    
+}
